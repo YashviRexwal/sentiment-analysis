74fae8006cbf9386bdeda651f5c86871543ede52
t4958639-prog <t4958639@gmail.com>
2026-05-07 08:10:05 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import streamlit as st 
 
+st.title("Streamlit Analysis Dashboard")
+st.write("Enter text to analyze sentiment")
+
+user_input = st.text_area("Enter your text")
+
+if st.button("Analyze"):
+    st.write ("Analyzing...")
